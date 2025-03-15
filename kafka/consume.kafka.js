eb81a9ecff2ffce226afd2543ad6f38392f4f62a
Rohan Sharma <rsharma7_be22@thapar.edu>
2025-03-15 17:17:35 +0530

    feat:riderService consuming done

--- kafka/consume.kafka.js
+++ kafka/consume.kafka.js
@@ -5,25 +5,35 @@ class ConsumingRiderClass {
   constructor() {
     this.kafka = new Kafka({
       clientId: DummykafkaConfig.clientId,
-      brokers: DummykafkaConfig.brokers,
-      topics: DummykafkaConfig.topics.acceptedOrder
+      brokers: DummykafkaConfig.brokers
     })
     this.consumer = this.kafka.consumer({ groupId: 'lol' })
+    this.isConnected = false;
   }
-
+  async connect() {
+    try {
+        if (!this.isConnected) {
+            await this.consumer.connect();
+            this.isConnected = true;
+            console.log('consumer connected successfully');
+        }
+    } catch (error) {
+        console.error('consumer connection failed:', error);
+        throw error;
+    }
+}
   async consumerfunction() {
-    await this.consumer.connect()
-    console.log("connected")
-    await this.consumer.subscribe({
+    const c=await this.connect()
+    const b=await this.consumer.subscribe({
       topics: [DummykafkaConfig.topics.acceptedOrder],
       fromBeginning: true
     })
-    console.log("subscribed",DummykafkaConfig.topics.acceptedOrder)
+    console.log("subscribed",b)
     const a = await this.consumer.run({
-      eachMessage: async ({ topics, message }) => {
+      eachMessage: async ({ topic,partition, message }) => {
         console.log(message)
         console.log({
-          topics: topics.toString(),
+          topic: topic.toString(),
           key: message.key.toString(),
           value: message.value.toString()
         })
--- worker/event.resolver.js
+++ worker/event.resolver.js
@@ -5,7 +5,6 @@ async function consumingMessages(){
     let count=0;
     const a=await consume.consumerfunction()
     console.log("count:",count,typeof a)
-    console.log("count:",count,JSON.parse(a))
 }
 
 consumingMessages()
